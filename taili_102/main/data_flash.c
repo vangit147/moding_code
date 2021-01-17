871a3bbb1b1f309efa45d6f62201799ca59f950b
van -home <316340525@qq.com>
2021-01-18 05:26:04 +0800

    need to fix time to wakeup by timer

--- taili_102/main/data_flash.c
+++ taili_102/main/data_flash.c
@@ -183,26 +183,34 @@ void updated_esp_time()
 	time_now=stime.tv_sec;
 	p=localtime(&time_now);
 	printf("p->tm_hour=%d p->tm_min=%d p->tm_sec=%d ",p->tm_hour,p->tm_min,p->tm_sec);
-	if(p->tm_hour==12&&p->tm_min==59&&p->tm_sec>=20&&p->tm_sec<=59)
+//	if(p->tm_hour==12&&p->tm_min==59&&p->tm_sec>=20&&p->tm_sec<=59)
+	int HOUR=12;
+	int MIN=0;
+	if(p->tm_hour==HOUR&&p->tm_min==MIN&&p->tm_sec>=20&&p->tm_sec<=59)
 	{
 		ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(600*1000*1000));//10 minutes
 		ESP_LOGW(timer_tag,"After hours 10 minutes , it will be wakeup by timer");
 	}
-	else
+	else if(p->tm_min<=MIN)
 	{
 		int ti;
-		if(p->tm_hour<=12)
+		if(p->tm_hour<=HOUR)
 		{
-			ti=12-p->tm_hour-1;
-			ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup((ti*60*60+(60-p->tm_min)*60+60-p->tm_sec)*1000*1000));
-			ESP_LOGW(timer_tag,"After %d hours %d minutes %d seconds , it will be wakeup by timer",ti,60-p->tm_min,60-p->tm_sec);
+			ti=HOUR-p->tm_hour;
+			ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup((ti*60*60+(MIN-p->tm_min-1)*60+60-p->tm_sec)*1000*1000));
+			ESP_LOGW(timer_tag,"After %d hours %d minutes %d seconds , it will be wakeup by timer",ti,MIN-p->tm_min,60-p->tm_sec);
 		}
 		else
 		{
-			ti=36-p->tm_hour-1;
-			ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup((ti*60*60+(60-p->tm_min)*60+60-p->tm_sec)*1000*1000));
-			ESP_LOGW(timer_tag,"After %d hours %d minutes %d seconds , it will be wakeup by timer",ti,60-p->tm_min,60-p->tm_sec);
+			ti=24+HOUR-p->tm_hour;
+			ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup((ti*60*60+(MIN-p->tm_min-1)*60+60-p->tm_sec)*1000*1000));
+			ESP_LOGW(timer_tag,"After %d hours %d minutes %d seconds , it will be wakeup by timer",ti,MIN-p->tm_min,60-p->tm_sec);
 		}
+	}
+	else
+	{
+		ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup((23*60*60+(60-p->tm_min-1)*60+60-p->tm_sec)*1000*1000));
+		ESP_LOGW(timer_tag,"After 23 hours %d minutes %d seconds , it will be wakeup by timer",(60-p->tm_min-1),60-p->tm_sec);
 	}
 }
 
