5fb3cff7832bb0677baea21b78cd49a8466f03a9
van <316340525@qq.com>
2021-01-22 18:15:21 +0800

    add ULP

---
+++ ULP/ulp_v3/ulp_adc/build/esp-idf/main/ulp_main/CMakeFiles/3.13.4/CMakeASMCompiler.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_ASM_COMPILER "D:/esp2/.espressif/tools/esp32ulp-elf/2.28.51-esp-20191205/esp32ulp-elf-binutils/bin/esp32ulp-elf-as.exe")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "D:/esp2/.espressif/tools/esp32ulp-elf/2.28.51-esp-20191205/esp32ulp-elf-binutils/bin/esp32ulp-elf-ar.exe")
+set(CMAKE_ASM_COMPILER_AR "CMAKE_ASM_COMPILER_AR-NOTFOUND")
+set(CMAKE_RANLIB "D:/esp2/.espressif/tools/esp32ulp-elf/2.28.51-esp-20191205/esp32ulp-elf-binutils/bin/esp32ulp-elf-ranlib.exe")
+set(CMAKE_ASM_COMPILER_RANLIB "CMAKE_ASM_COMPILER_RANLIB-NOTFOUND")
+set(CMAKE_LINKER "D:/esp2/.espressif/tools/esp32ulp-elf/2.28.51-esp-20191205/esp32ulp-elf-binutils/bin/esp32ulp-elf-ld.exe")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
---
+++ ULP/ulp_v3/ulp_adc/build/esp-idf/main/ulp_main/ulp_main.h
@@ -0,0 +1,33 @@
+// Variable definitions for ESP32ULP
+// This file is generated automatically by esp32ulp_mapgen.py utility
+
+#pragma once
+
+extern uint32_t ulp_ac;
+extern uint32_t ulp_acl;
+extern uint32_t ulp_ay;
+extern uint32_t ulp_az;
+extern uint32_t ulp_azl;
+extern uint32_t ulp_compare_times;
+extern uint32_t ulp_comparex;
+extern uint32_t ulp_entry;
+extern uint32_t ulp_exit;
+extern uint32_t ulp_i2c_read_bit;
+extern uint32_t ulp_i2c_read_byte;
+extern uint32_t ulp_i2c_start_cond;
+extern uint32_t ulp_i2c_stop_cond;
+extern uint32_t ulp_i2c_write_bit;
+extern uint32_t ulp_i2c_write_byte;
+extern uint32_t ulp_init_mpu;
+extern uint32_t ulp_m_ax;
+extern uint32_t ulp_m_az;
+extern uint32_t ulp_move_flags;
+extern uint32_t ulp_moving_times;
+extern uint32_t ulp_p_ax;
+extern uint32_t ulp_p_az;
+extern uint32_t ulp_read_intro;
+extern uint32_t ulp_stack;
+extern uint32_t ulp_stackEnd;
+extern uint32_t ulp_sysrun_times;
+extern uint32_t ulp_wake_up;
+extern uint32_t ulp_write8;
